8619cf10ae4848c9aa602384d7dd554cf2e2d635
kuoai1994 <1334508254@qq.com>
2018-05-24 15:06:31 +0800

    添加注释

--- 爬虫/test1.py
+++ 爬虫/test1.py
@@ -1,10 +1,11 @@
 import urllib.request
-url="http://www.douban.com/"
-request=urllib.request.Request(url)
-response=urllib.request.urlopen(request)
+url="http://www.douban.com/"        #网址
+request=urllib.request.Request(url)     #请求
+response=urllib.request.urlopen(request)        #爬取结果
 data=response.read()
-data=data.decode("utf-8")
-print(data)
+data=data.decode("utf-8")       #设置解码方式
+print(data)         #打印结果
+# 打印爬去网页的各类信息
 print(type(response))
 print(response.geturl())
 print(response.info())
