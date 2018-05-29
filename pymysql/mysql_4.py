30bc96fe8d84fef60e76f322e9a91d911692f430
kuoai <133450824@qq.com>
2018-05-29 10:40:46 +0800

    pymysql 删除数据库

---
+++ pymysql/mysql_4.py
@@ -0,0 +1,19 @@
+import pymysql
+# 删除操作
+
+# 打开数据库
+db = pymysql.connect(host="127.0.0.1",user="root",password="123456",db="hmh")
+
+
+# 使用cursou()方法获取操作游标
+cur = db.cursor()
+
+sql_delete = "delete form user where id = %d"
+try:
+    cur.execute(sql_delete % (3))
+    db.commit()
+except Exception as e:
+    db.rollback()
+finally:
+    db.close()
+
