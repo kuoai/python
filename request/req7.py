d6e64ce21e39ad22fbd25a6ab52f443c17fa54bb
kuoai <133450824@qq.com>
2018-05-29 17:55:56 +0800

    re findall

---
+++ request/req7.py
@@ -0,0 +1,30 @@
+import re
+# findall       搜索整个字符串，然后返回匹配正则表达式所有的内容
+html = '''<div id="songs-list">
+    <h2 class="title">经典老歌</h2>
+    <p class="introduction">
+        经典老歌列表
+    </p>
+    <ul id="list" class="list-group">
+        <li data-view="2">一路上有你</li>
+        <li data-view="7">
+            <a href="/2.mp3" singer="任贤齐">沧海一声笑</a>
+        </li>
+        <li data-view="4" class="active">
+            <a href="/3.mp3" singer="齐秦">往事随风</a>
+        </li>
+        <li data-view="6"><a href="/4.mp3" singer="beyond">光辉岁月</a></li>
+        <li data-view="5"><a href="/5.mp3" singer="陈慧琳">记事本</a></li>
+        <li data-view="5">
+            <a href="/6.mp3" singer="邓丽君"><i class="fa fa-user"></i>但愿人长久</a>
+        </li>
+    </ul>
+</div>'''
+results = re.findall('<li.*?href="(.*?)".*?singer="(.*?)">(.*?)</a>',html,re.S)
+# print(results)
+for result in results:
+    # print(result)
+    print(result[0],result[1],result[2])
+
+# 如果只是获取第一个内容，可以用search()方法
+# 当需要提取多个内容时，可以用findall()方法
