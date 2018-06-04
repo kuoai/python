baae1f9bba5759f8751091866e8fd8311c6edb72
kuoai <133450824@qq.com>
2018-06-04 11:36:33 +0800

    Django，模板

--- Django/Django/view.py
+++ Django/Django/view.py
@@ -1,3 +1,15 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
+# def hello(request):
+#     return HttpResponse("hello world!")
+
+
+from django.shortcuts import render
 def hello(request):
-    return HttpResponse("hello world!")
+    context = {}
+    context["hello"] = "你好"
+    return render(request,"hello.html",context)
+
+# 这里使用render()来代替之前的HttpResponse
+# render()还使用了一个字典，context作为参数
+# context字典元素的键值“hello”对应了模板中的变量{{hello}}
+
