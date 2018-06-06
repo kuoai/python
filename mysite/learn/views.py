6cf31cdc5dffb07eb8c8069afa108c2d02eae652
kuoai <133450824@qq.com>
2018-06-06 14:33:55 +0800

    mysite，GET

--- mysite/learn/views.py
+++ mysite/learn/views.py
@@ -1,5 +1,16 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
-def index(request):
-    return HttpResponse(u"<h1>你还年轻吗？<h1/>")
+
+# 第一种方法采用 /add/?a=4&b=5 这样GET方法进行
+def add(request):
+    a = request.GET['a']
+    b = request.GET['b']
+    c = int(a) * int(b)
+    return HttpResponse(str(c))
+
+
+# 第二种方法 采用 /add/3/4/ 这样的网址的方式
+def add2(request,a,b):
+    c = int(a) * int(b)
+    return HttpResponse(str(c))
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,23 +1,9 @@
-"""mysite URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
 from learn import views as learn_views  # new
 
 urlpatterns = [
-    path('', learn_views.index),  # new
-    path('admin/', admin.site.urls),
+    url(r'^add/$', learn_views.add, name='add'),  # 注意修改了这一行
+    url(r'^add/(\d+)/(\d+)/$', learn_views.add2, name='add2'),
+    url(r'^admin/', admin.site.urls),
 ]
