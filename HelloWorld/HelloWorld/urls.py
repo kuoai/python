4540b348f21b4169fbc663c606685d880e1bc8ce
kuoai <133450824@qq.com>
2018-06-06 13:49:38 +0800

    Django，admin工具

--- HelloWorld/HelloWorld/urls.py
+++ HelloWorld/HelloWorld/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 
 from . import view,search,search_post
+from django.contrib import admin
 
 urlpatterns = [
     url(r'^hello$', view.hello),
     url(r'^search_form$', search.search_form),
     url(r'^search$', search.search),
-    url(r'^search_post$',search_post.search_post)
+    url(r'^search_post$',search_post.search_post),
+    url(r'^admin/',admin.site.urls)
 ]
