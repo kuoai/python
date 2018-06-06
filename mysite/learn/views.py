b90192ac9baf593e7eb47d17ab2a4113155f4c56
kuoai <133450824@qq.com>
2018-06-06 15:01:36 +0800

    mysite，转换地址

--- mysite/learn/views.py
+++ mysite/learn/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponsePermanentRedirect
+from django.urls import reverse
 
 
 # 第一种方法采用 /add/?a=4&b=5 这样GET方法进行
@@ -14,3 +16,14 @@ def add(request):
 def add2(request,a,b):
     c = int(a) * int(b)
     return HttpResponse(str(c))
+
+
+# 使用模板
+def index(request):
+    return render(request,"home.html")
+
+# 跳转函数
+def old_add2_redirect(request,a,b):
+    return HttpResponsePermanentRedirect(
+        reverse("add2",args=(a,b))
+    )
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -3,7 +3,12 @@ from django.contrib import admin
 from learn import views as learn_views  # new
 
 urlpatterns = [
+    url(r'^$',learn_views.index, name="home"),
     url(r'^add/$', learn_views.add, name='add'),  # 注意修改了这一行
-    url(r'^add/(\d+)/(\d+)/$', learn_views.add2, name='add2'),
+
+    url(r'^add/(\d+)/(\d+)/$', learn_views.old_add2_redirect),
+    url(r'^new_add/(\d+)/(\d+)/$',learn_views.add2, name='add2'),
+
+
     url(r'^admin/', admin.site.urls),
 ]
